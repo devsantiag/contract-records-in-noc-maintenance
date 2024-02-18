376a8bee5b896b7223e636fee4605f45280e7773
santiago <98044979+devsantiag@users.noreply.github.com>
2024-02-18 18:00:44 -0300

    Add files via upload

--- boxRegistro_nocContratos/scripts/contractRegistrations/contractRegistrationForm.js
+++ boxRegistro_nocContratos/scripts/contractRegistrations/contractRegistrationForm.js
@@ -7,28 +7,31 @@ function register() {
 
     if (inputValues(empresaNome, empresaDesignador, inputTime, inputDate)) {
         addPost(empresaNome, empresaDesignador, inputTime, inputDate);
-        dateValidation(inputDate);
-    }
-}
+        clearInputField();
+        recordContracts();
+    };
+};
 
 // Certifica se todos os campos de entrada INPUT estão preenchidos 
-async function inputValues(empresaNome, empresaDesignador, inputTime, inputDate) {
+function inputValues(empresaNome, empresaDesignador, inputTime, inputDate) {
     if (empresaNome.trim() === '' || empresaDesignador.trim() === '' || inputTime.trim() === '' || inputDate.trim() === '') {
         alert('Preencha todas as informações corretamente para prosseguir com o registro do contrato.');
         return false;
-    }
+    };
     return true;
-}
+};
 
 // Formaliza a postagem para o HTML
-async function addPost(empresaNome, empresaDesignador, inputTime, inputDate) {
+let count = 0;
+function addPost(empresaNome, empresaDesignador, inputTime, inputDate) {
+    count++;
     const post = document.getElementById('post');
     const postItem = document.createElement('div');
     postItem.className = 'post';
-    postItem.innerHTML = `Manutenção: ${empresaNome} - contrato: ${empresaDesignador}. Finalização e/ou previsão ${inputTime} - mês ${inputDate}`;
+    postItem.innerHTML = `(${count}) - Empresa em manutenção: ${empresaNome}, contrato de identidicação: ${empresaDesignador}, data de finalização prevista para às  ${inputTime} do mês ${inputDate}`;
     post.appendChild(postItem);
     dateValidation(inputDate) && postItem.classList.add('past-date');
-}
+};
 
 // Adiciona instrução
 function dateValidation(inputDate) {
@@ -36,6 +39,25 @@ function dateValidation(inputDate) {
         const selectedDate = new Date(inputDate);
         return selectedDate < new Date();
     } catch (error) {
-        alert(`Ocorreu um erro ao processar a data. Por favor,  tente novamente. Erro: ${error}`)
+        alert(`Ocorreu um erro ao processar a data. Por favor,  tente novamente. Erro: ${error}`);
     };
+};
+
+// limpa os campos inputs pós-registro 
+function clearInputField() {
+    document.getElementById('empresaNome').value = '';
+    document.getElementById('empresaDesignador').value = '';
+    document.getElementById('inputTime').value = '';
+    document.getElementById('inputDate').value = '';
+};
+
+// mostra o total de contratos registrados
+let contractCount = 0;  // Inicializado com 0
+recordContracts = () => {
+    const countContracts = document.getElementById('countContracts');
+    let newElement = document.createElement('p');
+    contractCount++;
+    countContracts.innerHTML = '';
+    newElement.textContent = `Total de registros: ${contractCount}`;
+    countContracts.appendChild(newElement);
 };
